1fb6211c541342ee0a174f81c9dc9b7c4afe1715
Pierre Mavro <deimos@deimos.fr>
2020-04-23 23:21:55 +0200

    feat: first release
    g

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # oled_backlight
-Linux OLED backlight management for Linux
+Linux OLED backlight management for Linux intel cards
+
+I've a Lenovo X1 Carbon Extreme Gen2 and recently the OLED support has been fixed (thanks to https://gitlab.freedesktop.org/drm/intel/issues/510)
+
+Tested on Arch linux (Intel Corporation UHD Graphics 630):
+* kernel 5.6.6-arch1-1
+* xf86-video-intel 1:2.99.917+906+g846b53da-1
+
+However common softs like xbacklight doesn't recognize the screen. So I've made this quick program in order to control backlight through the command line. See usage:
+
+
+```
+# print help
+$ ./oled_backlight
+Please add one of those argument: current|+|-|XY%
+
+# return current percentage
+$ ./oled_backlight current
+50
+
+# lighter +5%
+$ sudo ./oled_backlight +
+55
+
+# darker -5%
+$ sudo ./oled_backlight -
+50
+
+# set light percentage
+$ sudo ./oled_backlight 60%
+60
+```
+
+Note: it needs sudo permissions in order to modify the brightness
---
+++ oled_backlight.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func getCurrentBacklight(brightnessPath string) string {
+    data, err := ioutil.ReadFile(brightnessPath)
+    if err != nil {
+        fmt.Println("File reading error", err)
+        return "0"
+    }
+    return strings.TrimSuffix(string(data), "\n")
+}
+
+func getCurrentBacklightPercentage(backlightNumber int) string {
+    max := 2000.0
+    if backlightNumber == 0 {
+        return "0%"
+    }
+    backlightNumberFloat := float64(backlightNumber)
+    percentage := int((backlightNumberFloat / max) * 100)
+    return strconv.Itoa(percentage)
+}
+
+func setCurrentBacklightPercentage(backlightNumber string) int {
+    percentage, _ := strconv.Atoi(strings.Split(backlightNumber, "%")[0])
+    if percentage >= 100 {
+        percentage = 100
+    } else if percentage <= 0 {
+        percentage = 0
+    }
+    return percentage * 20
+}
+
+func help() {
+    fmt.Print("Please add one of those argument: current|+|-|XY%")
+}
+
+func main() {
+    var currentBrightness, setBrightness int
+    brightnessPath := "/sys/class/backlight/intel_backlight/brightness"
+
+    if len(os.Args) != 2 {
+        help()
+        os.Exit(1)
+    }
+
+    currentBrightness, _ = strconv.Atoi(getCurrentBacklight(brightnessPath))
+    if os.Args[1] == "current" {
+        fmt.Println(getCurrentBacklightPercentage(currentBrightness))
+        os.Exit(0)
+    } else if os.Args[1] == "+" {
+        if currentBrightness >= 2000 {
+            setBrightness = 2000
+        } else {
+            setBrightness = currentBrightness + 100
+        }
+    } else if os.Args[1] == "-" {
+        if currentBrightness <= 0 {
+            setBrightness = 0
+        } else {
+            setBrightness = currentBrightness - 100
+        }
+    } else if strings.Contains(os.Args[1], "%") {
+        setBrightness = setCurrentBacklightPercentage(os.Args[1])
+    } else {
+       help()
+       os.Exit(1)
+    }
+
+    fmt.Println(getCurrentBacklightPercentage(setBrightness))
+    f, err := os.Create(brightnessPath)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    _, err = f.WriteString(strconv.Itoa(setBrightness))
+    if err != nil {
+        fmt.Println(err)
+        f.Close()
+        return
+    }
+    err = f.Close()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+}
